c58215b4ad96ec130a6a69f5b78953982135d83c
eugeniekwon <eugenie.kwon@gmail.com>
2020-11-10 06:14:12 +0900

    second attempt of logo
    - fixed: kerning, transition to dot
    - need: wing direction and transparency, circle into a semicircle, timing
    - added new kit files for other layers

--- dev/js/logo.js
+++ dev/js/logo.js
@@ -1,17 +1,38 @@
 import {gsap} from "gsap";
 
+//logo wordmark forming
 
 gsap.set("#logo-wordmark", {transformOrigin: "50% 50%"})
 
 const logoTL = gsap.timeline();
 
-logoTL.from("#logo-wordmark", {opacity: 0, duration: 1})
-        .fromTo("#logo-wordmark", {scale: 5}, {duration: 2, scale: 1})
-        .fromTo("#logo-circle", {opacity: 0}, {opacity: 1, duration: 2})
-        .fromTo("#left-wing", {opacity: 0, x:120}, {opacity: 1, duration: 2, x: 0}, "wing") 
-        .fromTo("#right-wing", {opacity: 0, x:-120}, {opacity: 1, duration: 2, x: 0}, "wing"); 
+logoTL.fromTo("#logo-m", {opacity: 0, x: -10}, {opacity: 1, duration: 1, x: 0}, "wordmark")
+        .fromTo("#logo-i", {opacity: 0, x: -5}, {opacity: 1, duration: 1, x: 0}, "wordmark")
+        .fromTo("#logo-n", {opacity: 0, x: 5}, {opacity: 1, duration: 1, x: 0}, "wordmark")
+        .fromTo("#logo-ii", {opacity: 0, x: 10}, {opacity: 1, duration: 1, x: 0}, "wordmark")
+        .fromTo("#logo-wordmark", {scale: 3}, {duration: 1, scale: 1}, "-=0.5")
+        .fromTo("#logo-circle", {opacity: 0}, {opacity: 1, duration: 1})
+        .fromTo("#left-wing", {opacity: 0, x:120}, {opacity: 1, duration: 1, x: 0}, "wing") 
+        .fromTo("#right-wing", {opacity: 0, x:-120}, {opacity: 1, duration: 1, x: 0}, "wing"); 
 
 export function logoAnimation(){
     return logoTL;
 }
 
+
+//logo wordmark into a small circle
+
+const logocircleTL = gsap.timeline();
+
+gsap.set("#logo-circle", {transformOrigin: "50% 50%"})
+
+logocircleTL.to("#left-wing", {opacity: 0, duration: 0.2}, "wing") 
+            .to("#right-wing", {opacity: 0, duration: 0.2}, "wing")
+
+            .to("#logo-wordmark", {opacity: 0, duration: 0.5})
+            .to("#logo-circle", {scale: 0.2, duration: 0.5}, "-=0.3")
+
+
+export function logocircleAnimation(){
+    return logocircleTL;
+}
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,8 +2,9 @@ import {gsap} from "gsap";
 
 
 import {logoAnimation} from "./logo.js";
+import {logocircleAnimation} from "./logo.js";
 
 const mainTL = gsap.timeline();
 
 mainTL.add(logoAnimation())
-
+        .add(logocircleAnimation())
